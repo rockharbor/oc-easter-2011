748b62c9463fc5e7471264bf7eb7a048b0568426
Jeremy Harris <jharris@rockharbor.org>
2011-03-22 10:15:40 -0700

    Added scroll follow effect

--- js/timeline.js
+++ js/timeline.js
@@ -3,12 +3,13 @@
 	$.oceaster = {
 
 		init: function() {
+			$.oceaster._insection = $('section:first');
 			$('[data-scrolltrigger]').each(function(i) {
 				var self = $(this);
 				self.data('oceaster.start', Number(self.attr('data-triggerstart')));
 				self.data('oceaster.end', Number(self.attr('data-triggerend')));
-				if ($.oceaster['section'+(i+1)+'Init'] != undefined) {
-					$.oceaster['section'+(i+1)+'Init'](self);
+				if ($.oceaster[self.attr('data-scrolltrigger')+'Init'] != undefined) {
+					$.oceaster[self.attr('data-scrolltrigger')+'Init'](self);
 				}
 			});
 			$.oceaster.update();
@@ -32,7 +33,7 @@
 			});
 		},
 
-		section1Init: function(section) {
+		section1TriggerInit: function(section) {
 			var c = 1;
 			for (var i=1; i<7; i++) {
 				var clone = $('#opacsplit').clone();
@@ -72,8 +73,54 @@
 				}
 			});
 		},
+
+
+		html5Trigger1Init: function(section) {	$.oceaster._html5triggerInit(section);	},
+		html5Trigger1: function(section, percent) { $.oceaster._html5trigger(section, percent); },
+		html5Trigger2Init: function(section) { $.oceaster._html5triggerInit(section); },
+		html5Trigger2: function(section, percent) { $.oceaster._html5trigger(section, percent); },
+		html5Trigger3Init: function(section) { $.oceaster._html5triggerInit(section);	},
+		html5Trigger3: function(section, percent) { $.oceaster._html5trigger(section, percent); },
+		html5Trigger4Init: function(section) { $.oceaster._html5triggerInit(section);	},
+		html5Trigger4: function(section, percent) { $.oceaster._html5trigger(section, percent); },
+
+		_html5triggerInit: function(section) {
+			var p = $('p', section);
+			p.data('starttop', p.position().top);
+		},
+		_html5trigger: function(section, percent) {
+			var p = $('p', section);
+			if ($.oceaster.withinSection(section)) {
+				p.css({
+					top: p.data('starttop') + 200*percent
+				});
+
+				$('#html5 p').data('html5ptop', p.offset().top);
+			}
+		},
+
+		html5TriggerInit : function(section) {
+			var p = $('p', section);
+			p.data('starttop', p.position().top);
+			p.css({
+				opacity: 0
+			});
+		},
 		
-		section2Trigger: function(section, percent) {
+		html5Trigger: function(section, percent) {
+			if (!$.oceaster.withinSection(section)) {
+				$('#html5 p').data('html5ptop', -100);
+			}
+			var p = $('p', section);
+			p.css({
+				top: p.data('html5ptop'),
+				opacity: 1
+			});
+		},
+
+		withinSection: function(section) {
+			var scrollY = $(window).scrollTop();
+			return scrollY > section.data('oceaster.start') && scrollY < section.data('oceaster.end');
 		}
 
 	}	
