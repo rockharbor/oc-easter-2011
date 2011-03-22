83545e01ccd819f85fbd812554a69e6d9d0b8417
Jeremy Harris <jharris@rockharbor.org>
2011-03-22 09:07:25 -0700

    Added opacity split effect

--- js/timeline.js
+++ js/timeline.js
@@ -13,12 +13,19 @@
 					$.oceaster['section'+(i+1)+'Init'](self);
 				}
 			});
+			$.oceaster.update({}, true);
 		},
 
-		update: function(event) {
+		update: function(event, force) {
+			if (force == undefined) { 
+				force = false;
+			}
 			var scrollY = $(window).scrollTop();
 			$('#debug').html(scrollY);
 			$('[data-scrolltrigger]').filter(function() {
+				if (force) {
+					return true;
+				}
 				var self = $(this);
 				if (scrollY > self.data('oceaster.start') - $.oceaster.threshold && scrollY < self.data('oceaster.end') + $.oceaster.threshold) {
 					return true;
@@ -27,8 +34,8 @@
 				var self = $(this);
 				var amt = self.data('oceaster.end') - self.data('oceaster.start');
 				var scrolled = scrollY - self.data('oceaster.start');
-				var percent = (scrolled - amt) / amt;
-				if (percent < 1) {
+				var percent = scrolled / amt;
+				if (percent < 0) {
 					percent = 0;
 				}
 				if (percent > 1) {
@@ -44,16 +51,39 @@
 				var clone = $('#opacsplit').clone();
 				clone.css({
 					top: $('#opacsplit').position().top + (i%2>0 ? 10*c : -10*c),
-					opacity: 1 - (i%2>0 ? (i+1)/7 : i/7)
-				});
+					opacity: .7 - c*10/100
+				}).removeAttr('id').addClass('opacsplit');
 				section.append(clone);
 				if (i%2==0) {
 					c++;
 				}
+				clone.data('starttop', clone.position().top);
 			}
+			$('#opacsplit').css({visibility: 'hidden'});
 		},
 
-		section1Trigger: function(section, percent) {			
+		section1Trigger: function(section, percent) {
+			var dest = $('#opacsplit');
+			$('div.opacsplit', section).each(function(i) {
+				var self = $(this);
+				var dist = dest.position().top - self.data('starttop');
+				if (percent < .5) {
+					self.css({
+						top: self.data('starttop') + percent/.5*dist,
+						visibility: 'visible'
+					});
+					$('#opacsplit').css({
+						visibility: 'hidden'
+					});
+				} else {
+					self.css({
+						visibility: 'hidden'
+					});
+					$('#opacsplit').css({
+						visibility: 'visible'
+					});
+				}
+			});
 		},
 		
 		section2Trigger: function(section, percent) {
