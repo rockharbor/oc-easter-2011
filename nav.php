46a476cfcfce461f59b4578f4505a9e249455e1c
Jeremy Harris <jharris@rockharbor.org>
2011-03-28 07:52:54 -0700

    Fixed links

--- nav.php
+++ nav.php
@@ -1,11 +1,14 @@
 <nav data-scrolltrigger="navTrigger" data-triggerstart="100" data-triggerend="15000">
 	<h1>Menu</h1>
 	<ul>
-		<li><a href="about">About</a></li>
-		<li><a href="what-to-expect">What to expect</a></li>
-		<li><a href="why-we-celebrate">Why we celebrate</a></li>
-		<li><a href="kids">Kids</a></li>
-		<li><a href="church-directory">Church directory</a></li>
-		<li><a href="directions">Directions</a></li>
+		<?php	if (isset($_GET['url']) && $_GET['url'] != 'scroll'): ?>
+		<li><a href="/">Home</a></li>
+		<?php endif; ?>
+		<li><a href="/about">About</a></li>
+		<li><a href="/what-to-expect">What to expect</a></li>
+		<li><a href="/why-we-celebrate">Why we celebrate</a></li>
+		<li><a href="/kids">Kids</a></li>
+		<li><a href="/church-directory">Church directory</a></li>
+		<li><a href="/directions">Directions</a></li>
 	</ul>
 </nav>
