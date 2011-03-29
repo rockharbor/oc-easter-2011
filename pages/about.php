af11ca6c8a06618aee5c3395ff1c42d14a35a268
jeremyharris <jeremy@42pixels.com>
2011-03-29 11:31:43 -0700

    Added facebook link

--- pages/about.php
+++ pages/about.php
@@ -9,5 +9,6 @@
 		</p>
 		<p>A day to celebrate. Thousands will gather to reflect on, to mourn, and to worship Jesus. His story of grace, mercy, and freedom will be told and celebrated. Whether you have attended church for years or have never stepped foot inside one, come experience the historic events and powerful message of Easter in a personal and significant way.</p>
 		<p>Services are open to everyone.</p>
+		<p><a href="http://www.facebook.com/event.php?eid=208676585810729" class="button" target="_blank">RSVP or find out more on Facebook</a></p>
 	</article>
 </section>
