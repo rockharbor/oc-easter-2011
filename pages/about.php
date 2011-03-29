554ddffe6e80b3fb89ee89f6f35974208c9d1c19
jeremyharris <jeremy@42pixels.com>
2011-03-29 11:50:59 -0700

    Content change on about page

--- pages/about.php
+++ pages/about.php
@@ -7,7 +7,7 @@
 		<p class="easter">
 			Easter at the Pacific Amphitheatre,	Sunday, April 24 9am & 11:15am
 		</p>
-		<p>A day to celebrate. Thousands will gather to reflect on, to mourn, and to worship Jesus. His story of grace, mercy, and freedom will be told and celebrated. Whether you have attended church for years or have never stepped foot inside one, come experience the historic events and powerful message of Easter in a personal and significant way.</p>
+		<p>A day to celebrate. Thousands will gather to reflect on, to celebrate, and to worship Jesus. His story of grace, victory, and freedom will be told and celebrated. Whether you have attended church for years or have never stepped foot inside one, come experience the historic events and powerful message of Easter in a personal and significant way.</p>
 		<p>Services are open to everyone.</p>
 		<p><a href="http://www.facebook.com/event.php?eid=208676585810729" class="button" target="_blank">RSVP or find out more on Facebook</a></p>
 	</article>
