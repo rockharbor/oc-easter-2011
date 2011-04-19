7d27e9b8dd572cbdaa49701e0616bb3cba43be98
Jeremy Harris <jharris@rockharbor.org>
2011-04-19 09:15:34 -0700

    Updated church directory

--- pages/church-directory.php
+++ pages/church-directory.php
@@ -3,215 +3,225 @@
 		<header>
 			<h1>Church Directory</h1>
 		</header>
-		<p>After celebrating Easter together, we encourage you to find a church in your local neighborhood. Worship together. Celebrate together. Walk with Jesus together.</p>
+		<p>Following Jesus is not intended to be done in isolation. After celebrating Easter together, we encourage you to find a church in your local neighborhood. Worship together. Celebrate together. Walk with Jesus together. </p>
 		<p>Check out this list of great churches in Orange and L.A. counties:</p>
 		<div class="clearfix">
 			<div style="float: left;width: 47%;padding-right: 10px">
+				<h2 style="display: block; margin-bottom: 10px;text-align: center;">Orange County</h2>
 				<h2>Brea</h2>
 	<div class="church"><h3>Southlands Church International</h3>
 	<p>714-982-5055<br />
 		<a target="_blank" class="button" href="http://www.southlands.net">www.southlands.net</a></p>
-	<p>meets at: <br />
-	2590 E. Imperial Hwy.<br />
-	Brea, CA 92821</p>
-	<p>weekend service times:<br />
-	Sundays | 6pm</p>
+	<p>2590 E. Imperial Hwy., Brea</p>
+	<p>Sundays | 9am &amp; 11am</p>
 	</div>
 	<h2>Costa Mesa</h2>
 	<div class="church"><h3>The Crossing Church</h3>
 	<p>949-645-5050<br />
 		<a target="_blank" class="button" href="http://www.thecrossing.com">www.thecrossing.com</a></p>
-	<p>meets at:<br />
-	2115 Newport Blvd.<br />
-	Costa Mesa, CA 92627</p>
-	<p>weekend service times:<br />
-	Saturdays | 5:30 pm<br />
-	[junior high ministry available]<br />
-	Sundays | 8:30am, 9:30am, 10am, 11am &amp; 11:30am<br />
-	[junior high and high school ministry available 10am & 11:30am]<br />
-	Children's Program<br />
-	Nursery – grade 5 at all services</p>
+	<p>2115 Newport Blvd., Costa Mesa</p>
+	<p>Saturdays | 5:30 pm<br />
+Sundays | 8:30am, 9:30am, 10am, 11am &amp; 11:30am
+</p>
 	</div>
 	<div class="church"><h3>Newport Mesa Church</h3>
 	<p>714-966-0454<br />
 		<a target="_blank" class="button" href="http://www.newportmesa.org">www.newportmesa.org</a></p>
-	<p>meets at:<br />
-	2599 Newport Blvd.<br />
-	Costa Mesa, CA  92627</p>
-	<p>weekend service times:<br />
-	Sundays | 9am &amp; 11am</p>
+	<p>2599 Newport Blvd., Costa Mesa</p>
+	<p>Sundays | 9am &amp; 11am</p>
 	</div>
-	<div class="church"><h3><strong>ROCK</strong>HARBOR</h3>
+	<div class="church"><h3><strong>ROCK</strong>HARBOR Central</h3>
 	<p>714-384-0914<br />
 		<a target="_blank" class="button" href="http://www.rockharbor.org">www.rockharbor.org</a></p>
-	<p><strong>ROCK</strong>HARBOR Central<br />
-	345 Fischer Avenue<br />
-	Costa Mesa, CA 92626</p>
-	<p>weekend celebration services:<br />
-	Saturdays | 6pm<br />
-	[children&apos;s ministry available for infants – 4 years old]</p>
-	<p>Sundays | 9am, 11am, 7pm, 9pm<br />
-	[children&apos;s ministry available for infants – grade 5 at 9am & 11am only]<br />
-	[middle school and high school ministry at 9am]</p>
-	<p><strong>ROCK</strong>HARBOR South County<br />
-		Laguna Hills High School<br />
-	25401 Paseo de Valencia<br />
-	Laguna Hills, CA 92653<br />
-	Sundays | 10am<br />
-	[children's ministry available for infants – grade 5]</p>
-	<p><strong>ROCK</strong>HARBOR Fullerton<br />
-	Pacific Auditorium, Hope International University<br />
-	2500 Nutwood<br />
-	Fullerton, CA<br />
-	Sundays | 5pm &amp; 7:30pm<br />
-	[children&apos;s ministry available at 5pm]</p>
+	<p>345 Fischer Avenue, Costa Mesa</p>
+	<p>Saturdays | 6pm<br />
+Sundays | 9am, 11am, 7pm, 9pm
+</p>
+	</div>
+	<h2>Fullerton</h2>
+	<div class="church"><h3>New Song North OC</h3>
+	<p>714-278-8020<br />
+		<a target="_blank" class="button" href="http://noc.newsong.net">noc.newsong.net</a></p>
+	<p>Pacific Auditorium @ Hope International University<br />
+2500 Nutwood Ave., Fullerton
+</p>
+	<p>Sundays | 9:30am  &amp; 11:15am</p>
+	</div>
+	<div class="church"><h3><strong>ROCK</strong>HARBOR Fullerton</h3>
+	<p>714-384-0914<br />
+		<a target="_blank" class="button" href="http://fullerton.rockharbor.org">fullerton.rockharbor.org</a></p>
+	<p>Pacific Auditorium @ Hope International University<br />
+2500 Nutwood Ave., Fullerton
+</p>
+	<p>Sundays | 5pm &amp; 7:30pm</p>
+	</div>
+	<h2>Huntington Beach</h2>
+	<div class="church"><h3>Mariners Huntington Beach</h3>
+	<p>949-769-8100<br />
+		<a target="_blank" class="button" href="http://www.marinerschurch.org">www.marinerschurch.org</a></p>
+	<p>Golden West College Theatre Building<br />
+15744 Goldenwest St., Huntington Beach
+</p>
+	<p>Sundays | 9:30am</p>
+	</div>
+	<div class="church"><h3><strong>ROCK</strong>HARBOR Huntington Beach</h3>
+	<p>714-384-0914<br />
+		<a target="_blank" class="button" href="http://www.rockharbor.org">www.rockharbor.org</a></p>
+	<p>Rodgers Senior Center<br />
+1706 Orange Ave., Huntington Beach
+</p>
+	<p>Sundays | 10am</p>
 	</div>
 	<h2>Irvine</h2>
-	<div class="church"><h3>New Song</h3>
-	<p>949-477-0700<br />
-		<a target="_blank" class="button" href="http://Irvine.newsong.net">Irvine.newsong.net</a>
-	</p>
-	<p>meets at:<br />
-	Irvine<br />
-	18842 Teller Ave.<br />
-	Irvine, CA 92612</p>
-	<p>weekend service times:<br />
-	Sundays | 9am, 10:15am, &amp; 11:39am<br />
-	[children&apos;s ministry available 9am &amp; 10:15am, middle school ministry available at 9am, high school ministry available at 10:15am]</p>
-	<p>meets at:<br />
-	North OC<br />
-	505 E. Commonwealth Ave., Suita 100<br />
-	Fullerton, CA 92832</p>
-	<p>weekend service times:<br />
-	Sundays | 9:30am  &amp; 11:15am<br />
-	[children&apos;s ministry available 9:30am & 11:15am, middle school ministry available at 9:30am]</p>
-	</div>
 	<div class="church"><h3>Mariners</h3>
-	<p>949-854-7600<br />
-	<a target="_blank" class="button" href="http://www.marinerschurch.org">www.marinerschurch.org</a></p>
-	<p>meets at:<br />
-	5001 Newport Coast Dr.<br />
-	Irvine, CA 92603</p>
-	<p>weekend service times:<br />
-	Saturdays | 4:30pm and 6:30pm<br />
-	[children&apos;s ministry available 4:30pm &amp; 6:30pm, junior high and high school ministry available at 6:30pm]<br />
-	Sundays | 9am, 11 am &amp; 7pm<br />
-	[children&apos;s, junior high and high school ministry available 9am &amp; 11am]</p>
+	<p>949-769-8100<br />
+		<a target="_blank" class="button" href="http://www.marinerschurch.org">www.marinerschurch.org</a></p>
+	<p>5001 Newport Coast Dr., Irvine</p>
+	<p>Saturdays | 5pm<br />
+Sundays | 9am &amp; 11am</p>
+	</div>
+	<div class="church"><h3>Moment</h3>
+	<p>949-385-3373<br />
+		<a target="_blank" class="button" href="http://momentchurch.com">momentchurch.com</a></p>
+	<p>18842 Teller Ave., Irvine</p>
+	<p>Sundays | 6pm</p>
+	</div>
+	<div class="church"><h3>New Song Irvine</h3>
+	<p>949-477-0700<br />
+		<a target="_blank" class="button" href="http://irvine.newsong.net">irvine.newsong.net</a>
+	</p>
+	<p>18842 Teller Ave., Irvine</p>
+	<p>Sundays | 8:45am, 10:15am, &amp; 11:30am</p>
 	</div>
 	<div class="church"><h3>Pathways Church</h3>
-	<p>323-464-8455<br />
-		<a target="_blank" class="button" href="http://www.ecclesiahollywood.org">www.ecclesiahollywood.org</a></p>
-	<p>meets at:<br />
-	Hicks Canyon Elementary School<br />
-	3817 Viewpark<br />
-	Irvine, CA 92602</p>
-	<p>weekend service times:<br />
-	Sundays | 10am<br />
-	[children&apos;s ministry, middle school ministry, and high school ministry available at 10am]</p>
+	<p>714-543-4400<br />
+		<a target="_blank" class="button" href="http://ocpathways.org">ocpathways.org</a></p>
+	<p>Hicks Canyon Elementary School<br />
+3817 Viewpark, Irvine</p>
+	<p>Sundays | 10am</p>
 </div>
-
-			</div>
-			<div style="float: left;width: 47%;padding-left: 10px">
+	<h2>Laguna Hills</h2>
+	<div class="church"><h3><strong>ROCK</strong>HARBOR South County</h3>
+	<p>714-384-0914<br />
+		<a target="_blank" class="button" href="http://southcounty.rockharbor.org">southcounty.rockharbor.org</a>
+	</p>
+	<p>Laguna Hills High School<br />
+25401 Paseo De Valencia, Laguna Hills
+</p>
+	<p>Sundays | 10am</p>
+	</div>
+	<h2>Mission Viejo</h2>
+	<div class="church"><h3>Mariners Mission Viejo</h3>
+	<p>949-769-8100<br />
+		<a target="_blank" class="button" href="http://www.marinerschurch.org">www.marinerschurch.org</a>
+	</p>
+	<p>26862 Crown Valley Pkwy., Mission Viejo</p>
+	<p>Saturdays | 5pm<br />Sundays | 9am &amp; 11am </p>
+	</div>
 	<h2>Orange</h2>
 	<div class="church"><h3>Beacon Church</h3>
-	<p>714-363-0257<br />
+	<p>714-564-1601<br />
 		<a target="_blank" class="button" href="http://www.thebeaconoc.org">www.thebeaconoc.org</a></p>
-	<p>meets at:<br />
-	El Modena High School<br />
-	3920 Spring Street<br />
-	Orange, CA 92869</p>
-	<p>weekend service times:<br />
-	Sundays | 10am<br />
-	[children&apos;s ministry available for infants – grade 6]</p>
+	<p>El Modena High School<br />
+3920 Spring St., Orange
+</p>
+	<p>Sundays | 10:05am</p>
+	</div>
+	<div class="church"><h3>Mars Hill Orange County</h3>
+	<p><a target="_blank" class="button" href="http://www.marshillchurch.org">www.marshillchurch.org</a></p>
+	<p>Sunday Gathering Times &amp; Locations TBD</p>
 	</div>
 	<h2>Yorba Linda</h2>
 	<div class="church"><h3>Yorba Linda Friends</h3>
 	<p>714-777-2875<br />
-		<a target="_blank" class="button" href="http://www.ylfc.org">www.ylfc.org</a></p>
-	<p>meets at:<br />
-	5141 Lakeview Avenue<br />
-	Yorba Linda, CA 92886</p>
-	<p>weekend service times:<br />
-	Saturdays | 6pm<br />
-	Sundays | 9am &amp; 10:45am<br />
-	[children&apos;s ministry provided at all services, junior high and high school ministry available at 9am]</p>
+		<a target="_blank" class="button" href="http://www.friendschurchyl.com">www.friendschurchyl.com</a></p>
+	<p>5141 Lakeview Ave., Yorba Linda</p>
+	<p>Saturdays | 6pm <br />
+Sundays | 9am &amp; 10:45am
+</p>
 	</div>
+			</div>
+			<div style="float: left;width: 47%;padding-left: 10px">
+	
+	<h2 style="display: block; margin-bottom: 10px;text-align: center;">Los Angeles County</h2>
 	<h2>Long Beach</h2>
+	<div class="church"><h3>The Garden</h3>
+	<p><a target="_blank" class="button" href="http://www.thegardenlb.org">www.thegardenlb.org</a></p>
+	<p>Cohiba Night Club<br />110 E. Broadway, Long Beach</p>
+	<p>Sundays | 10am</p>
+	</div>
 	<div class="church"><h3>Grace Brethren</h3>
 	<p>562-595-6881<br />
-		<a target="_blank" class="button" href="http://www.gracelb.org">www.gracelb.org</a></p>
-	<p>meets at:<br />
-	36th and Linden<br />
-	Long Beach, CA 90807</p>
-	<p>weekend service times:<br />
-	Sundays | 9:30am</p>
+		<a target="_blank" class="button" href="http://www.gracelb.net">www.gracelb.net</a></p>
+	<p>36th &amp; Linden, Long Beach</p>
+	<p>Sundays | 9:30am</p>
 	</div>
 	<h2>Los Angeles</h2>
+	<div class="church"><h3>Basileia</h3>
+	<p>323-793-5732<br />
+		<a target="_blank" class="button" href="http://www.basileiacommunity.org">www.basileiacommunity.org</a></p>
+	<p>4903 Fountain Ave., Hollywood</p>
+	<p>Sundays | 10:30am<br />Thursdays | 7:30pm</p>
+	</div>
 	<div class="church"><h3>Ecclesia</h3>
 	<p>323-464-8455<br />
-		<a target="_blank" class="button" href="http://www.ecclesiahollywood.org">www.ecclesiahollywood.org</a></p>
-	<p>meets at:<br />
-	The Hollywood Pacific Theatre<br />
-	6433 Hollywood Blvd.<br />
-	Hollywood, CA  90028</p>
-	<p>weekend service times:<br />
-	Sundays | 11am</p>
+		<a target="_blank" class="button" href="http://www.churchinhollywood.com">www.churchinhollywood.com</a></p>
+	<p>The Hollywood Pacific Theatre<br />6433 Hollywood Blvd., Hollywood</p>
+	<p>Sundays | 9:30 &amp; 11:15am</p>
 	</div>
-	<div class="church"><h3>Mosaic</h3>
+	<div class="church"><h3>Mosaic Downtown L.A.</h3>
 	<p>626-628-9640<br />
 		<a target="_blank" class="button" href="http://www.mosaic.org">www.mosaic.org</a></p>
-	<p>meets at:<br />
-	West L.A.<br />
-	Beverly Hills High School<br />
-	241 Moreno Dr.<br />
-	Beverly Hills, CA</p>
-	<p>weekend service times:<br />
-	Sundays | 11:15am<br />
-	[children&apos;s ministry available for birth – grade 5]</p>
-	<p>Pasadena<br />
-	William Carey University (Mott)<br />
-	1539 East Howard Street<br />
-	Pasadena, CA</p>
-	<p>weekend service times:<br />
-	Sundays | 9:30am &amp; 11:15am<br />
-	[children&apos;s and high school ministry available, junior high ministry not available]</p>
-	<p>Downtown L.A.<br />
-	The Mayan Theatre<br />
-	1038 South Hill Street<br />
-	Los Angeles, CA</p>
-	<p>weekend service times:<br />
-	Sundays | 6pm &amp; 8pm<br />
-	[no children&apos;s programming]</p>
-	<p>Inland<br />
-	5540 Schaefer Avenue<br />
-	Chino, CA</p>
-	<p>weekend service times:<br />
-	Sundays | 10am<br />
-	[children&apos;s – high school ministry available]</p>
-	<p>Whittier<br />
-	6737 Bright Avenue<br />
-	Whittier, CA 90601</p>
-	<p>weekend service times:<br />
-	Sundays | 10am<br />
-	[children&apos;s – high school ministry available]</p>
-	<p>South Bay<br />
-	Knob Hill Community Center<br />
-	320 Knob Hill<br />
-	Redondo Beach, CA 90277</p>
-	<p>weekend service times:<br />
-	Saturdays | 6:15 pm<br />
-	[children&apos;s ministry available for toddler – grade 5]</p>
+	<p>The Mayan Theatre<br />1038 South Hill Street, L.A.</p>
+	<p>Sundays | 7pm</p>
 	</div>
-	<div class="church"><h3>Reality LA</h3>
+	<div class="church"><h3>Mosaic West L.A.</h3>
+	<p>626-628-9640<br />
+		<a target="_blank" class="button" href="http://www.mosaic.org">www.mosaic.org</a></p>
+	<p>Beverly Hills High School<br />241 Moreno Dr.<br />Beverly Hills, Ca</p>
+	<p>Sundays | 5pm</p>
+	</div>
+	<div class="church"><h3>New Song L.A.</h3>
+	<p>310-807-5795<br />
+		<a target="_blank" class="button" href="http://la.newsong.net">la.newsong.net</a></p>
+	<p>Animo Leadership Charter High School<br />1155 W. Arbor Vitae St., Inglewood</p>
+	<p>Sundays | 9:30am &amp; 11:30am</p>
+	</div>
+	<div class="church"><h3>Reality L.A.</h3>
 	<p>213-221-7672<br />
 		<a target="_blank" class="button" href="http://www.realityla.com">www.realityla.com</a></p>
-	<p>meets at:<br />
-	Helen Bernstein High School<br />
-	1309 N. Wilton Place<br />
-	Hollywood, CA  90028</p>
-	<p>weekend service times:<br />
-	Sundays | 10am</p>
-			</div>
+	<p>Helen Bernstein High School<br />1309 N. Wilton Pl., Hollywood</p>
+	<p>Sundays | 9am, 12pm, 5pm</p>
+	</div>
+	<h2>Pasadena</h2>
+	<div class="church"><h3>Mosaic Pasadena</h3>
+	<p>626-628-9640<br />
+		<a target="_blank" class="button" href="http://www.mosaic.org">www.mosaic.org</a></p>
+	<p>William Carey University<br />1539 East Howard St., Pasadena</p>
+	<p>Sundays | 10am</p>
+	</div>
+	<h2>Pomona</h2>
+	<div class="church"><h3>Mosaic Pomona</h3>
+	<p>626-628-9640<br />
+		<a target="_blank" class="button" href="http://www.mosaic.org">www.mosaic.org</a></p>
+	<p>The Fox Theater<br />301 South Garey Ave., Pomona</p>
+	<p>Sundays | 10am</p>
+	</div>
+	<h2>Redondo Beach</h2>
+	<div class="church"><h3>Mosaic South Bay</h3>
+	<p>626-628-9640<br />
+		<a target="_blank" class="button" href="http://www.mosaic.org">www.mosaic.org</a></p>
+	<p>Knob Hill Community Center<br />320 Knob Hill, Redondo Beach</p>
+	<p>Sundays | 5pm</p>
+	</div>
+	<h2>Whittier</h2>
+	<div class="church"><h3>Mosaic Whittier</h3>
+	<p>626-628-9640<br />
+		<a target="_blank" class="button" href="http://www.mosaic.org">www.mosaic.org</a></p>
+	<p>7320 Greenleaf Ave., Whittier</p>
+	<p>Sundays | 10am</p>
+	</div>
+	
 		</div>
 	</div>
 	</article>
